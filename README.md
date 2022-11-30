a57bce90b2817f749ea3bd25757d34cf804af10d
Ishita <ishitajindal0023@gmail.com>
2022-11-30 23:46:30 +0530

    my_final_commit_for_this_project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Tree-NoTree-Forecasting
-
-
--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def model_predict(img_path,model):
     yhat = model.predict(np.expand_dims(re_size/255, 0))
     print(yhat)
     if yhat < 0.5: 
-        return 'tree'
+        return 'No Tree'
     else:
-        return 'no Tree'
+        return 'Tree'
 
 @app.route('/',methods=['GET'])
 def index():
@@ -40,26 +40,12 @@ def upload():
 
         # save the file to uploads folder
         
-        #basepath=os.path.dirname(os.path.realpath('__file__'))
-        #file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
-        #f.save(file_path)
+        basepath=os.path.dirname(os.path.realpath('__file__'))
+        file_path=os.path.join(basepath,'uploads',secure_filename(f.filename))
+        f.save(file_path)
         
-        # Make prediction
-        folder_dir = r'C:\Users\91896\Documents\projects\19_sept_hackathon\Chitkara_Unseen_Images_3_200'
-        for images in os.listdir(folder_dir):
-            if (images.endswith(".png")):
-                result = model_predict(folder_dir,model)
-                
-                if result == "Tree":
-                        answer = 1
-                else:
-                        answer = 0
-                        
-                list_data=[secure_filename(f.filename), answer]
-                with open('CSVFILE.csv', 'a', newline='') as f_object: 
-                    writer_object = writer(f_object)
-                    writer_object.writerow(list_data)  
-                    f_object.close()
+        #result
+        result = model_predict(file_path, model)
         return result
     return None
 
